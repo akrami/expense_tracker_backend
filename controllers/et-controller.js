3c01b2f814493ba39f2867240abfd79972902dfe
Alireza Akrami <akrami.ui@gmail.com>
2020-04-21 02:47:52 +0430

    sort

--- controllers/et-controller.js
+++ controllers/et-controller.js
@@ -1,48 +1,48 @@
 const Expense = require('../models/expense');
 
 const getExpenses = (req, res, next) => {
-    Expense.find()
-    .exec()
-    .then(data=>{
-        res.json(data);
-    });
+    Expense.find().sort({ when: -1 })
+        .exec()
+        .then(data => {
+            res.json(data);
+        });
 }
 
 const addExpense = (req, res, next) => {
     const { category, description, amount, when } = req.body;
-    
-    const expense = new Expense({ category, description, amount, when})
-    
+
+    const expense = new Expense({ category, description, amount, when })
+
     expense.save()
-    .then(()=>{
-        res.json({
-            status: 1
+        .then(() => {
+            res.json({
+                status: 1
+            });
+        })
+        .catch(error => {
+            res.json({
+                status: 0,
+                error
+            });
         });
-    })
-    .catch(error=>{
-        res.json({
-            status: 0,
-            error
-        });
-    });
 }
 
 const incomeCategories = (req, res, next) => {
-    Expense.distinct("category",{
-        amount: {$gt : 0}
+    Expense.distinct("category", {
+        amount: { $gt: 0 }
     }).exec()
-    .then(data=>{
-        res.json(data);
-    });
+        .then(data => {
+            res.json(data);
+        });
 }
 
 const expenseCategories = (req, res, next) => {
-    Expense.distinct("category",{
-        amount: {$lt : 0}
+    Expense.distinct("category", {
+        amount: { $lt: 0 }
     }).exec()
-    .then(data=>{
-        res.json(data);
-    });
+        .then(data => {
+            res.json(data);
+        });
 }
 
 exports.getExpenses = getExpenses;
