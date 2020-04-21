b2e8e85784e1fc928a99806a44d5bd34d8422cc6
Alireza Akrami <akrami.ui@gmail.com>
2020-04-22 01:57:10 +0430

    last30days

--- controllers/et-controller.js
+++ controllers/et-controller.js
@@ -39,6 +39,23 @@ const addExpense = (req, res, next) => {
         });
 }
 
+const getCategories = (req, res, next) => {
+    Expense.distinct("category").exec()
+        .then(data => {
+            res.json(data);
+        });
+}
+
+const getCategorySum = (req, res, next) => {
+    Expense.aggregate([{
+        $group: {
+            _id: "$category",
+            total: { $sum: "$amount" }
+        }
+    }]).exec()
+        .then(data => res.json(data));
+}
+
 const incomeCategories = (req, res, next) => {
     Expense.distinct("category", {
         amount: { $gt: 0 }
@@ -57,8 +74,61 @@ const expenseCategories = (req, res, next) => {
         });
 }
 
+const last30Days = (req, res, next) => {
+    let now = new Date();
+    let aimDate = new Date();   
+    now.setUTCHours(0);
+    now.setUTCMinutes(0);
+    now.setUTCSeconds(0);
+    now.setUTCMilliseconds(0);
+    aimDate.setDate(aimDate.getDate() - 30);
+    aimDate.setUTCHours(0);
+    aimDate.setUTCMinutes(0);
+    aimDate.setUTCSeconds(0);
+    aimDate.setUTCMilliseconds(0);
+
+    const timeDiff = (now - aimDate)/86400000;
+
+    Expense.aggregate([
+        { $match: { "when": { $gte: aimDate, $lte: now } } },
+        {
+            $addFields: {
+                when: {
+                    $dateFromParts: { year: { $year: "$when" }, month: { $month: "$when" }, day: { $dayOfMonth: "$when" } }
+                },
+                dateRange: {
+                    $map: {
+                        input: {
+                            $range: [0, timeDiff]
+                        },
+                        as: "mlt",
+                        in: {$add:[aimDate, {$multiply:["$$mlt",86400000]}]}
+                    }
+                }
+            }
+        },
+        { $unwind: "$dateRange" },
+        {
+            $group: {
+                _id: "$dateRange",
+                count: {
+                    $sum: {
+                        $cond: [{ $eq: ["$dateRange", "$when"] }, "$amount", 0]
+                    }
+                }
+            }
+        },
+        { $sort: { _id: 1 } }
+    ]).exec()
+        .then(data => res.json(data))
+        .catch(error=>console.error(error));
+}
+
 exports.getExpenses = getExpenses;
 exports.getTotal = getTotal;
 exports.addExpense = addExpense;
+exports.getCategories = getCategories;
+exports.getCategorySum = getCategorySum;
 exports.incomeCategories = incomeCategories;
 exports.expenseCategories = expenseCategories;
+exports.last30Days = last30Days;
--- routes/et-routes.js
+++ routes/et-routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const { getExpenses, addExpense, incomeCategories, expenseCategories, getTotal } = require('../controllers/et-controller');
+const { getExpenses, addExpense, incomeCategories, expenseCategories, getTotal, getCategorySum, getCategories, last30Days } = require('../controllers/et-controller');
 
 const router = express.Router();
 
 router.get('/', getExpenses);
 router.post('/', addExpense);
 router.get('/total', getTotal);
+router.get('/categories', getCategories);
+router.get('/categories/total', getCategorySum);
 router.get('/categories/income', incomeCategories);
 router.get('/categories/expense', expenseCategories);
+router.get('/data/last30days', last30Days);
 
 module.exports = router;
