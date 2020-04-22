d8dcd687f7baf4d7963e7f5e624e845f906bf4b5
Alireza Akrami <akrami.ui@gmail.com>
2020-04-22 16:14:51 +0430

    timezone friendly

--- controllers/et-controller.js
+++ controllers/et-controller.js
@@ -76,16 +76,17 @@ const expenseCategories = (req, res, next) => {
 
 const last30Days = (req, res, next) => {
     let now = new Date();
-    let aimDate = new Date();   
-    now.setUTCHours(0);
-    now.setUTCMinutes(0);
-    now.setUTCSeconds(0);
-    now.setUTCMilliseconds(0);
+    let aimDate = new Date(); 
+    now.setDate(now.getDate()+1);
+    now.setHours(0);
+    now.setMinutes(0);
+    now.setSeconds(0);
+    now.setMilliseconds(0);
     aimDate.setDate(aimDate.getDate() - 30);
-    aimDate.setUTCHours(0);
-    aimDate.setUTCMinutes(0);
-    aimDate.setUTCSeconds(0);
-    aimDate.setUTCMilliseconds(0);
+    aimDate.setHours(0);
+    aimDate.setMinutes(0);
+    aimDate.setSeconds(0);
+    aimDate.setMilliseconds(0);
 
     const timeDiff = (now - aimDate)/86400000;
 
@@ -93,16 +94,13 @@ const last30Days = (req, res, next) => {
         { $match: { "when": { $gte: aimDate, $lte: now } } },
         {
             $addFields: {
-                when: {
-                    $dateFromParts: { year: { $year: "$when" }, month: { $month: "$when" }, day: { $dayOfMonth: "$when" } }
-                },
                 dateRange: {
                     $map: {
                         input: {
                             $range: [0, timeDiff]
                         },
                         as: "mlt",
-                        in: {$add:[aimDate, {$multiply:["$$mlt",86400000]}]}
+                        in: {min:{$add:[aimDate, {$multiply:["$$mlt",86400000]}]}, max: {$add:[aimDate, {$multiply:[{$add:["$$mlt",1]},86400000]}]}}
                     }
                 }
             }
@@ -111,14 +109,15 @@ const last30Days = (req, res, next) => {
         {
             $group: {
                 _id: "$dateRange",
-                count: {
+                total: {
                     $sum: {
-                        $cond: [{ $eq: ["$dateRange", "$when"] }, "$amount", 0]
+                        $cond: [{ $and: [ { $gte: [ "$when", "$dateRange.min" ] }, { $lt: [ "$when", "$dateRange.max" ] } ] }, "$amount", 0]
                     }
                 }
             }
         },
-        { $sort: { _id: 1 } }
+        { $sort: { _id: 1 } },
+        { $project: { "date":"$_id.min", "total":1, "_id":0}}
     ]).exec()
         .then(data => res.json(data))
         .catch(error=>console.error(error));
